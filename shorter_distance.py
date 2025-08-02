3213b957c573a059ca8a174f878dc373ccf24759
BLAISE <122952796+bla24@users.noreply.github.com>
2025-08-02 20:35:17 +0200

    Update shorter_distance.py

--- shorter_distance.py
+++ shorter_distance.py
@@ -1,3 +1,5 @@
+#Algorithms are step-by-step procedures that developers use to perform calculations and solve computational problems.
+#In this project, you'll learn how to use functions, loops, conditional statements, and dictionary comprehensions to implement a Shortest Path algorithm.
 my_graph = {
     'A': [('B', 5), ('C', 3), ('E', 11)],
     'B': [('A', 5), ('C', 1), ('F', 2)],
